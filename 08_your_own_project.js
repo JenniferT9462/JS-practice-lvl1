77b2ccee7f88ce19160b5e18ccf51b69add07306
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-06-28 22:21:26 -0500

    Deleted old lessons

--- 08_your_own_project.js
+++
@@ -1,9 +0,0 @@
-// 🧠 8. Make Your Own
-
-// Challenge students to remix what they’ve learned:
-
-//     Create a mini game.
-
-//     Ask a question and give feedback.
-
-//     Make a button that changes a score or color.
